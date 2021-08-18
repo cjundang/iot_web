6484e71eb791b058dfe14a3a77fb31b7f6edd617
Chanankorn Jandaeng <cjundang@gmail.com>
2021-08-19 06:48:35 +0700

    add arduino

---
+++ arduino_code/iot_server.ino
@@ -0,0 +1,53 @@
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP8266     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+#include <ESP8266WiFi.h>
+
+#include "DHT.h"
+
+WiFiServer server(80);
+
+DHT dht4(4,DHT11);
+
+void setup()
+{
+  Serial.begin(9600);
+  dht4.begin();
+  WiFi.disconnect();
+  delay(3000);
+   WiFi.begin("SSID","PASSWORD");
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    Serial.print(".");
+    delay(300);
+
+  }
+  Serial.println("Connected");
+  Serial.print("IP Address: ");
+  Serial.println((WiFi.localIP().toString()));
+  server.begin();
+  
+
+}
+
+
+void loop()
+{
+
+    WiFiClient client = server.available();
+    if (!client) { return; }
+    while(!client.available()){  delay(1); }
+    Serial.print("Request from ");
+    Serial.println((client.remoteIP()));
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("");
+    client.println("<!DOCTYPE HTML>");
+    client.println("<html>");
+    client.println((dht4.readTemperature( )));
+    client.println("</html>");
+
+    delay(1);
+
+}
